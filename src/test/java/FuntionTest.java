a3c6b394b5d50782aa7058faad9dcc10ffa6a05f
DoubleK <airvw14@gmail.com>
2022-06-13 21:55:35 +0900

    refactor(Function<T, R>): Test
    Function<T, R> Test with lambda

--- src/test/java/FuntionTest.java
+++ src/test/java/FuntionTest.java
@@ -2,15 +2,25 @@ import function.Func;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FuntionTest {
 
+    private final int num = 10;
+
     @Test
     @DisplayName("람다식 적용 X")
     void funtion(){
-        int num = 10;
         Func func = new Func();
         assertThat(func.apply(10)).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("람다식 적용 O")
+    void functionLambda(){
+        Function<Integer, Integer> func = (i) -> i * 2;
+        assertThat(func.apply(num)).isEqualTo(20);
     }
 }
