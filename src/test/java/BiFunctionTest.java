980ac4ca5e732ce6e52a0bd3f297b4e246e7ac21
DoubleK <airvw14@gmail.com>
2022-06-15 21:15:06 +0900

    refactor(BiFunction<T, R>): test
    Change BiFunction<T, R> test with lambda
    
    BREAKING CHANGE: change BiFunctionTestTest.java
    - use method reference

--- src/test/java/BiFunctionTest.java
+++ src/test/java/BiFunctionTest.java
@@ -1,4 +1,5 @@
 import function.BiFunc;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,18 +11,23 @@ public class BiFunctionTest {
 
     private final String hello = "안녕";
     private final String name = "더 자바";
+    private BiFunc biFunc;
+
+    @BeforeEach
+    void setUp(){
+        biFunc = new BiFunc();
+    }
 
     @Test
     @DisplayName("람다식 적용X")
     void biFunction(){
-        BiFunc biFunc = new BiFunc();
         assertThat(biFunc.apply(hello, name)).isEqualTo("안녕 더 자바");
     }
 
     @Test
     @DisplayName("람다식 적용")
     void biFunctionLambda(){
-        BiFunction<String, String, String> biFunction = (s1, s2) -> s1 + " " + s2;
+        BiFunction<String, String, String> biFunction = biFunc::apply;
         assertThat(biFunction.apply(hello, name)).isEqualTo("안녕 더 자바");
     }
 }
