436c69f9a865958cda67d60b972a52f1cf08cb85
DoubleK <airvw14@gmail.com>
2022-06-15 21:17:18 +0900

    refactor(Supplier<T>): test
    Change Supplier<T> test with lambda
    
    BREAKING CHANGE: changeSupplierTest.java
    - use method reference

--- src/test/java/SupplierTest.java
+++ src/test/java/SupplierTest.java
@@ -1,4 +1,5 @@
 import function.Supply;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,17 +9,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SupplierTest {
 
+    private Supply supply;
+
+    @BeforeEach
+    void setUp(){
+        supply = new Supply();
+    }
+
     @Test
     @DisplayName("람다식 적용X")
     void supplier(){
-        Supply get123 = new Supply();
-        assertThat(get123.get()).isEqualTo(123);
+        assertThat(supply.get()).isEqualTo(123);
     }
 
     @Test
     @DisplayName("람다식 적용")
     void supplierLambda(){
-        Supplier<Integer> get123 = () -> 123;
+        Supplier<Integer> get123 = supply::get;
         assertThat(get123.get()).isEqualTo(123);
     }
 }
