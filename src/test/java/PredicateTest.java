6be41a8934ad290a5ca90fcd2a501a798921bf23
DoubleK <airvw14@gmail.com>
2022-06-15 21:09:41 +0900

    refactor(Predicate<T>): test
    Change Predicate<T> test with lambda
    
    BREAKING CHANGE: change BinaryOperatorTest.java
    - use method
    reference
    
    change Predic.java
    - simplify if statement

--- src/test/java/PredicateTest.java
+++ src/test/java/PredicateTest.java
@@ -1,4 +1,5 @@
 import function.Predic;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,18 +10,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PredicateTest {
 
     private final int num = 44;
+    private Predic predic;
+
+    @BeforeEach
+    void setUp(){
+        predic = new Predic();
+    }
 
     @Test
     @DisplayName("람다식 적용X")
     void predicate(){
-        Predic predic = new Predic();
         assertThat(predic.test(num)).isEqualTo(true);
     }
 
     @Test
     @DisplayName("람다식 적용")
     void predicateLambda(){
-        Predicate<Integer> predicate = (i) -> i % 2 == 0;
+        Predicate<Integer> predicate = predic::test;
         assertThat(predicate.test(num)).isEqualTo(true);
     }
 }
