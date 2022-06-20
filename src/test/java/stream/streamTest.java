b491933c500de65e11728713feba305ab6a5bf0e
DoubleK <airvw14@gmail.com>
2022-06-21 00:36:15 +0900

    test(stream): test
    make test
    
    BREAKING CHANGE: new streamTest.java

---
+++ src/test/java/stream/streamTest.java
@@ -0,0 +1,25 @@
+package stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class streamTest {
+
+    @Test
+    @DisplayName("spring으로 시작하는 수업")
+
+    @Test
+    @DisplayName("close 되지 않은 수업")
+
+    @Test
+    @DisplayName("수업 이름만 모아서 스트림 만들기")
+
+    @Test
+    @DisplayName("두 수업 목록에 들어있는 모든 수업 아이디 출력")
+
+    @Test
+    @DisplayName("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만")
+
+    @Test
+    @DisplayName("자바 수업 중에 Test가 들어있는 수업이 있는지 확인")
+}
